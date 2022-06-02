e0bdac8e83b7d417f61f93f568b414a23a2063aa
Nastya Vertal <veeertal@yandex.ru>
2022-06-02 14:16:53 +0300

    test

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,3 @@
-from api_yamdb.settings import EMAIL_HOST
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
@@ -15,6 +14,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from api_yamdb.settings import EMAIL_HOST
 from reviews.models import Category, Genre, Review, Title
 
 from .filters import TitleFilter
