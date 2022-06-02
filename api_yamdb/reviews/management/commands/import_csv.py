ebddf3080c1292b33dc293ae4f7c3d83d74cb3a1
Nastya Vertal <veeertal@yandex.ru>
2022-06-02 12:39:21 +0300

    deploy test

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -4,6 +4,7 @@ import os
 from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+
 from reviews.models import Category, Genre, Review, Title
 
 User = get_user_model()
